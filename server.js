815e3fe64603be8f665eb70aa02e4f2045548a91
Umut Özkardeş <103787380+umutozkrds@users.noreply.github.com>
2025-07-07 08:51:13 +0300

    api request was changed and gold price pulled succesfully

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 const loadProducts = async () => {
     try {
-        const data = fs.readFileSync(__dirname, "data",  'products.json', 'utf8');
+        const data = fs.readFileSync(path.join(__dirname, 'data', 'products.json'), 'utf8');
         return JSON.parse(data);
     } catch (error) {
         console.error('Error loading products:', error);
@@ -32,11 +32,10 @@ const fetchGold = async () => {
         if (fetchedPrice) {
             return fetchedPrice;
         }
-        const response = await axios.get('https://api.metals.live/v1/spot/gold', {
+        const response = await axios.get('https://api.metalpriceapi.com/v1/latest?api_key=b5ff76dec7c7accb0c2a8de134a875a3&base=USD&currencies=XAG', {
             timeout: 10000,
         });
-        const price = response.data.price;
-        const goldPricePerGram = price / 31.1;
+        const goldPricePerGram = response.data.rates.XAG; 
         caches.set('goldPrice', goldPricePerGram);
         return goldPricePerGram;
     } catch (error) {
@@ -45,6 +44,42 @@ const fetchGold = async () => {
     }
 };
 
+const calculatePrice = (popularityScore, weight, goldPrice) => {
+        return ((popularityScore+ 1) * weight * goldPrice).toFixed(2);
+};
+
+const addCalculatedPrice = (products, index) => {
+    const goldPrice = caches.get('goldPrice');
+    if (!goldPrice) {
+        return;
+    }
+    const calculatedPrice = calculatePrice(products.popularityScore, products.weight, goldPrice);
+    return {
+        id : index + 1,
+        ...products,
+        price: calculatedPrice,
+    };
+};
+const addCalculatedPriceToProducts = (products) => {
+    return products.map(addCalculatedPrice);
+};
 
 
+app.get('/api/products', async (req, res) => {
+    try {
+        const products = await loadProducts();
+        const calculatedProducts = addCalculatedPriceToProducts(products);
+        res.json(calculatedProducts);
+    } catch (error) {
+        console.error('Error fetching products:', error);
+        res.status(500).json({ error: 'Failed to fetch products' });
+    }
+});
+
+
+app.listen(port, async () => {
+    console.log(`Server is running on port ${port}`);
+    const goldPrice = await fetchGold();
+    console.log(`Gold price: ${goldPrice}`);
+});
 
